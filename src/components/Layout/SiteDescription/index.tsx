a1052bc930ca15abe7d409ca28c329a7637c2d7a
selosele <osh3384@naver.com>
2024-01-23 11:02:55 +0900

    사이트 설명 컴포넌트 개발

---
+++ src/components/Layout/SiteDescription/index.tsx
@@ -0,0 +1,15 @@
+/** Site description component */
+export default function SiteDescription(props: SiteDescriptionProps) {
+  return (
+    <p className={`w-full text-center`}>
+      {props.name}
+    </p>
+  )
+}
+
+/** Site description props interface */
+interface SiteDescriptionProps {
+
+  /** Site description name */
+  name: string;
+}
