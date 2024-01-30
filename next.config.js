c0f9d4ccfe28e8e08d88bf131e30ab4a610260b9
selosele <osh3384@naver.com>
2024-01-30 12:07:43 +0900

    sass 사용 설정

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 const { withContentlayer } = require('next-contentlayer')
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `
+      @import "@/styles/variable.scss";
+    `,
+  },
 }
 
 module.exports = withContentlayer(nextConfig)
